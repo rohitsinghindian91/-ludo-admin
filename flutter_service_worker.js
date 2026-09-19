642a006faa3fcef30fcbc4876544416877f34b27
rohitsinghindian91 <rohitsinghindian91@users.noreply.github.com>
2026-09-19 17:04:12 +0000

    deploy: ab29eb9b7777787390dd786d9c643749e89505c1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"manifest.json": "f6f5141002cbbe1bfa43f0b2c548525e",
 "version.json": "1771c40d078c3dff15dd70af7770857f",
-"main.dart.js": "d1bd16ca8d422de710822b44fd00dedc",
-"flutter_bootstrap.js": "fb1666891dff93c22dd2466db963a767",
+"main.dart.js": "c4921e883242edf3a5b042c2caa81be8",
+"flutter_bootstrap.js": "77f0f9895f312f686587dc7bd8ff61e5",
 "canvaskit/skwasm.js": "694fda5704053957c2594de355805228",
 "canvaskit/chromium/canvaskit.wasm": "b1ac05b29c127d86df4bcfbf50dd902a",
 "canvaskit/chromium/canvaskit.js": "671c6b4f8fcc199dcc551c7bb125f239",
